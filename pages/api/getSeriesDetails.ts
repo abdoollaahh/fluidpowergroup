a60ee03fe7760e58d58f342ca439514fa097c33b
aaziz001 <aziz2001900@gmail.com>
2022-10-20 00:51:33 +0500

    bug fix

--- pages/api/getSeriesDetails.ts
+++ pages/api/getSeriesDetails.ts
@@ -10,7 +10,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (series !== null) {
       const seriesDetails = {
         name: series.name,
-        description: series.description !== null ? String(series.description).replaceAll("<br>", "") : "",
+        description: series.description !== null ? series.description : "",
         images: series.images !== null ? series.images.map((image: any) => image.file.url) : ["https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"]
       }
       res.status(200).json({series: seriesDetails})
