6ef29219da94afc9b3a09f23b345dc896830a116
Muhammad Abdullah <abdullahcch@gmail.com>
2022-11-29 17:24:50 +0500

    added text

--- components/views/About/HeaderAbout.tsx
+++ components/views/About/HeaderAbout.tsx
@@ -3,16 +3,14 @@ const HeaderAbout = () => {
     <div className="flex flex-col gap-4 lg:w-1/3  ">
       <h2 className=" text-4xl font-semibold">Who are we</h2>
       <main className="text-justify font-light text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        autem dolorem quod beatae esse nisi quis. Non voluptates iste provident
-        inventore quam porro, beatae et eum aperiam quibusdam vel autem sint
-        tenetur reprehenderit explicabo commodi, vero laborum, minus nobis
-        necessitatibus magni ipsa! Aliquam hic quod sunt amet eum. Saepe cum
-        ipsum eligendi mollitia. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Voluptatibus autem dolorem quod beatae esse nisi quis.
-        Non <br /> <br /> Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Voluptatibus autem dolorem quod beatae esse nisi quis. Non
-        voluptates iste provident inventore quam porro.
+        We offer wide range of hydraulic hose & fittings, adaptors, hydraulic
+        tubes and all other miscellaneous parts that are needed in the fluid and
+        hydraulic industry.<br></br> <br></br>Tube bending, flaring & cutting
+        ring assembly, pressure testing and hose crimping are our main services.
+        <br></br>
+        <br></br>We design and build hydraulic systems and circuits according to
+        your needs and requirements We are also into design and drafting
+        services. Bring us your idea and we will shape it into a reality
       </main>
     </div>
   );
--- components/views/Home/HeroHome/InfoHeroHome.tsx
+++ components/views/Home/HeroHome/InfoHeroHome.tsx
@@ -6,12 +6,11 @@ const InfoHeroHome = () => {
   return (
     <div className="md:w-1/2  h-full flex flex-col  justify-center    gap-4 items-center text-center md:text-left md:items-start ">
       <h1 className="text-3xl sm:text-4xl xl:text-5xl font-semibold">
-        Welcome to Fluidpower
+        Welcome to FluidPower
       </h1>
       <h2 className="text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-        laudantium sint velit eos veniam possimus impedit atque ratione et sed,
-        fugiat, alias itaque aut pariatur quia, nemo quasi dolorem recusandae?
+        A growing company investing in talented personnel & new technology to
+        deliver cutting edge services and products with competitive prices.
       </h2>
       <Anchor href="/catalogue" className="hover:no-underline">
         <button className="max-w-max pl-6 pr-4  lg:pl-8 lg:pr-6 flex items-center gap-2 mt-4 text-lg lg:text-xl hover:text-yellow-400">
