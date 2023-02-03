4ef54d3482b938d7b8282701446cbb12df5cee2e
Muhammad Abdullah <abdullahcch@gmail.com>
2023-02-04 00:18:12 +0500

    SubCategories

--- components/views/Products/HeaderProducts.tsx
+++ components/views/Products/HeaderProducts.tsx
@@ -1,21 +1,30 @@
-import { useEffect, useState } from "react";
-import { Category } from "types/products";
+import { useRouter } from "next/router";
+import { useEffect, useMemo, useState } from "react";
+import { Category, SubCategory } from "types/products";
 
-const HeaderProducts = ({categories, slug} : {categories: Category[], slug: string | string[] |undefined}) => {
-  const [description, setDescription] = useState(categories[0].description)
+const HeaderProducts = ({
+  categories,
+  slug,
+}: {
+  categories: Category[];
+  slug: string | string[] | undefined;
+}) => {
+  const { subcategory } = useRouter().query;
 
-  useEffect(() => {
-    console.log()
-    categories.forEach((category) => {
-      slug?.includes(category.slug) && setDescription(category.description)
-    })
-  }, [slug])
+  const subCategories = useMemo(
+    () =>
+      categories.reduce(
+        (prev: SubCategory[], curr) => prev.concat(curr.subCategories),
+        []
+      ),
+    []
+  );
 
   return (
     <div className="flex flex-col items-center gap-3">
       <h1 className="text-4xl font-semibold">Products</h1>
       <h2 className="text-xl font-light text-center">
-        {description}
+        {subCategories.find((c) => subcategory === c.slug)?.description}
       </h2>
     </div>
   );
