126f807c9e3d3a04161e8d7a2ad3d759ec8f7120
WahWahKhan <wahwahkhan@github.com>
2025-01-26 23:39:51 +1100

    Fix: Update Navigation component with correct TypeScript types and Link implementation

--- components/views/Navigation/index.tsx
+++ components/views/Navigation/index.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import Link from 'next/link';
-/// <reference types="react" />
+import React from 'react'
+import Link from 'next/link'
 
-const Navigation: React.FC = () => {
+const Navigation = (): JSX.Element => {
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <Link href="/" className="text-xl font-bold">
-                Fluid Power Group
+              <Link href="/" passHref>
+                <a className="text-xl font-bold">
+                  Fluid Power Group
+                </a>
               </Link>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-              <Link href="/" className="px-3 py-2 hover:text-blue-600">
-                Home
+              <Link href="/" passHref>
+                <a className="px-3 py-2 hover:text-blue-600">
+                  Home
+                </a>
               </Link>
-              <Link href="/hosebuilder" className="px-3 py-2 hover:text-blue-600">
-                Hose Builder
+              <Link href="/hosebuilder" passHref>
+                <a className="px-3 py-2 hover:text-blue-600">
+                  Hose Builder
+                </a>
               </Link>
             </div>
           </div>
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
