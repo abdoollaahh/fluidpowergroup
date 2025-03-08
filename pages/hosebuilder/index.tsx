1bb85178446d8e5bf361d1e9cfcc13f35d5ab954
WahWahKhan <wahwahkhan@github.com>
2025-03-08 17:12:10 +1100

    fix: Confirmation emails

--- pages/hosebuilder/index.tsx
+++ pages/hosebuilder/index.tsx
@@ -15,7 +15,7 @@ const HoseBuilder = () => {
     
     const scripts = [
       '/hosebuilder/static/js/52.74ea38c5.js',
-      '/hosebuilder/static/js/main.4fb44117.js'
+      '/hosebuilder/static/js/main.69ff52ab.js'
     ];
 
     const loadScriptSequentially = async (scripts: string[]) => {
