64b39e8d1aba09c839790cbcae5d61dedc6225c2
WahWahKhan <wahwahkhan@github.com>
2025-03-15 13:46:42 +1100

    fix for ebay comment animations

--- pages/hosebuilder/index.tsx
+++ pages/hosebuilder/index.tsx
@@ -15,7 +15,7 @@ const HoseBuilder = () => {
     
     const scripts = [
       '/hosebuilder/static/js/52.74ea38c5.js',
-      '/hosebuilder/static/js/main.02d661e4.js'
+      '/hosebuilder/static/js/main.22aced3a.js'
     ];
 
     const loadScriptSequentially = async (scripts: string[]) => {
