5ba9cb8b7021841aacd24a46af67e71d30b1fac0
WahWahKhan <wahwahkhan@github.com>
2025-05-13 08:52:34 +1000

    New PayPal API Integration

--- pages/hosebuilder/index.tsx
+++ pages/hosebuilder/index.tsx
@@ -14,8 +14,8 @@ const HoseBuilder = () => {
     window.__PUBLIC_PATH__ = publicRuntimeConfig.staticFolder || '/hosebuilder/static/';
     
     const scripts = [
-      '/hosebuilder/static/js/52.74ea38c5.js',
-      '/hosebuilder/static/js/main.ebe0afec.js'
+      '/hosebuilder/static/js/453.0c2a2d38.js',
+      '/hosebuilder/static/js/main.b94f72f8.js'
     ];
 
     const loadScriptSequentially = async (scripts: string[]) => {
