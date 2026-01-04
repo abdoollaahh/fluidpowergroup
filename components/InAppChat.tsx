d42a8242ea86adf5958ce339850212187bb41193
WahWahKhan <wahwahkhan@github.com>
2026-01-04 19:17:32 +1100

    Suite360 offline plus InApp chat fix

--- components/InAppChat.tsx
+++ components/InAppChat.tsx
@@ -40,7 +40,7 @@ const InAppChat: React.FC<InAppChatProps> = ({
   hideWhenCartOpen = false
 }) => {
   const router = useRouter();
-  const isPWAPage = router.pathname === '/suite360/hose360';
+  const isPWAPage = router.pathname === '/suite360';
   
   const [scale, setScale] = useState(1);
   const [modalOpacity, setModalOpacity] = useState(0);
